e848168cccfaf3db09ffbe4a363b65c107318e70
hoshimado <xingyanhuan@yahoo.co.jp>
2018-11-28 22:16:54 +0900

    2章での動作確認に不要なコードを削除。

--- chapter02/section2-3-2after/src/api/sql_db_io/index.js
+++ chapter02/section2-3-2after/src/api/sql_db_io/index.js
@@ -5,28 +5,5 @@
  */
 
 
-var _shaping = require("./shaping_param4db.js");
-exports.getShowObjectFromGetData = _shaping.getShowObjectFromGetData;
-exports.getDeleteObjectFromPostData = _shaping.getDeleteObjectFromPostData;
-exports.getInsertObjectFromPostData = _shaping.getInsertObjectFromPostData;
-
-
-
-var _crud = require("./sql_lite_db_crud.js");
-exports.createPromiseForSqlConnection = _crud.createPromiseForSqlConnection;
-exports.closeConnection = _crud.closeConnection;
-
-exports.setupTable1st = _crud.setupTable1st;
-
-exports.addNewUser = _crud.addNewUser;
-exports.getNumberOfUsers = _crud.getNumberOfUsers;
-exports.deleteExistUser = _crud.deleteExistUser;
-exports.isOwnerValid = _crud.isOwnerValid;
-
-exports.getNumberOfLogs = _crud.getNumberOfLogs;
-exports.addActivityLog2Database = _crud.addActivityLog2Database;
-exports.getListOfActivityLogWhereDeviceKey = _crud.getListOfActivityLogWhereDeviceKey;
-exports.deleteActivityLogWhereDeviceKey = _crud.deleteActivityLogWhereDeviceKey;
-
-
-
+// 未だ不要。後で実装。
+// ※利用先のモジュールのテストでは、スタブで置き換えるでの、無くても問題ない（依存が無いの望ましい）。
